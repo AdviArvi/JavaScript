cb3418332523975bc1d92d42112fcb0ce9a424b7
Shital <shital@Shital-PC.local>
2020-02-17 17:27:18 +0530

    DOM

---
+++ dom.js
@@ -0,0 +1,11 @@
+const pic=document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.add('cool');
+pic.classList.remove('cool');
+pic.classList.contains('open');
+pic.classList.toggle('open');
+pic.classList.add('round');
+pic.classList.toggle('round');
+
+
+console.log(pic.classList);
